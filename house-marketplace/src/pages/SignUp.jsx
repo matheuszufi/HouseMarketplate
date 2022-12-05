dbd6f31bf45605a83f3ea16678e395d3fbd9f144
Matheus Zufi Leite <matheuszufi@gmail.com>
2022-12-05 11:02:34 -0300

    update db

--- house-marketplace/src/pages/SignUp.jsx
+++ house-marketplace/src/pages/SignUp.jsx
@@ -1,17 +1,20 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {db} from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 function SignUp() {
     const [showPassword, setShowPassword] = useState(false)
     const [formData, setFormData] = useState({
+        name: '',
         email: '',
         password: ''
 
     })
 
-    const {email, password} = formData
+    const {name, email, password} = formData
 
     const navigate = useNavigate()
     const onChange = (e) => {
@@ -23,7 +26,30 @@ function SignUp() {
     }
 
     
-
+    const onSubmit = async (e) => {
+        e.preventDefault()
+    
+        try {
+          const auth = getAuth()
+    
+          const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+          )
+    
+          const user = userCredential.user
+    
+          updateProfile(auth.currentUser, {
+            displayName: name,
+          })
+    
+    
+          navigate('/')
+        } catch (error) {
+            console.log('error')
+        }
+      }
 
 
     return (
@@ -33,7 +59,8 @@ function SignUp() {
                     <p className='pageHeader'> Welcome Back!</p>
                 </header>
             
-                <form>
+                <form onSubmit={onSubmit}>
+                    <input type='name' className='nameInput' placeholder='Name' id='name' value={name} onChange={onChange} />
                     <input type='email' className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange} />
                 
                     <div className='passwordInputDiv'>
@@ -44,10 +71,10 @@ function SignUp() {
                 
                     <Link to='/forgot-password' className='forgotPasswordLink'> Forgot Password </Link>
 
-                    <div className="signInBar">
-                        <p className="signInText">Sign In</p>
+                    <div className="signUpBar">
+                        <p className="signUpText">Sign Up</p>
 
-                        <button className="signInButton">
+                        <button className="signUpButton">
                             <ArrowRightIcon fill='#f1f1f1' whidth='34px' height='34px' />
                         </button>
                     </div>
@@ -55,7 +82,7 @@ function SignUp() {
             
                 {/* GOOGLE AUTH */}
 
-                <Link to='/sign-in' className='registerLink'> Sign Up Instead</Link>
+                <Link to='/sign-in' className='registerLink'> Sign In Instead</Link>
             
             
             
